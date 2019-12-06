2d37625d614d26fe0fe9f98ad216f450deaf2c9e
Volodymyr Zomber <zombervv@gmail.com>
2019-12-06 19:21:58 +0200

    better version

--- src/preparations.c
+++ src/preparations.c
@@ -31,31 +31,39 @@ char	*getInput(int fd)
 
 void	md5Executor(t_md5 *o)
 {
-	ft_md5((uint8_t *)o->input, ft_strlen(o->input));
-	return ;
+	char *input;
 
+	ft_md5((uint8_t *)o->input, ft_strlen(o->input), o);
 	if (o->q)
 	{
-		ft_md5((uint8_t *)o->input, ft_strlen(o->input));
-		ft_printf("\n");
+		printHashMD5(o, 1);
 		return ;
 	}
 	if (o->p && !o->isFile)
 		ft_printf("%s\n", o->input);
 	if (o->isStr || o->isFile)
-		stringFileCase(o);
-	else
 	{
-		ft_md5((uint8_t *)o->input, ft_strlen(o->input));
-		ft_printf("\n");
+		o->isFile ? input = o->fileName : input = o->input;
+		if (o->r)
+		{
+			printfHashMD5(o, 0);
+			ft_printf(" %s\n", input);
+		}
+		else
+		{
+			ft_printf("MD5 \(%s\) = ", input);
+			printfHashMD5(o, 1);
+		}
 	}
+	else
+		printHashMD5(o, 1);
 }
 
 int	tryOpen(char *fileName, t_md5 *o)
 {
 	int	fd;
 
-	if (fd = open(fileName, O_RDONLY) <= 0)
+	if (fd = open(fileName, O_RDONLY) < 0)
 	{
 		ft_printf("%s: No such file or directory\n", fileName);
 		return (0);
@@ -66,30 +74,48 @@ int	tryOpen(char *fileName, t_md5 *o)
 	return (1);
 }
 
-void	ft_md5Init(int ac, char **av)
+void	checkFlagsMD5(int ac, char **av, t_md5 *o)
 {
-	int		i;
-	t_md5	o;
+	int	i;
 
 	i = 1;
-	if (ac == 2)
-		o.input = getInput(0);
 	while (++i < ac)
 	{
 		if (ft_strcmp(av[i], "-r") == 0)
-			o.r = 1;
+			o->r = 1;
 		else if (ft_strcmp(av[i], "-q") == 0)
-			o.q = 1;
+			o->q = 1;
 		else if (ft_strcmp(av[i], "-p") == 0)
-			o.p = 1;
+			o->p = 1;
 		else if (ft_strcmp(av[i], "-s") == 0 && ac >= i + 1)
 		{
-			o.input = av[i + 1];
-			o.isStr = 1;
+			ft_printf("must be here\n");
+			o->input = av[i + 1];
+			o->isStr = 1;
 		}
 		else
-			if (!tryOpen(av[i], &o))
+			if (!tryOpen(av[i], o))
 				break;
 	}
-	md5Executor(&o);
+}
+
+void	ft_md5Init(int ac, char **av)
+{
+	t_md5	o;
+
+	ft_bzero(&o, sizeof(o));
+	o.h[0] = 0x67452301;
+	o.h[1] = 0xefcdab89;
+	o.h[2] = 0x98badcfe;
+	o.h[3] = 0x10325476;
+	checkFlagsMD5(ac, av, &o);
+	if (!o.isStr && !o.isFile)
+		o.input = getInput(0);
+	if (ft_strlen(o.input))
+	{
+		md5Executor(&o);
+		printHashMD5(&o, 1);
+	}
+	else
+		ft_printf("Got no input.\n");
 }
